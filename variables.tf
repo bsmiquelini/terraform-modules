e80275bbfbd35036c5cbce23f70c695749ae8468
Bruno <185800889+bsmiquelini@users.noreply.github.com>
2024-10-25 16:21:56 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+  description = "Nome do Resource Group"
+}
+
+variable "location" {
+  type = string
+  description = "Região"
+}
+
+variable "storage_account_name" {
+  type = string
+  description = "Nome do storage Account"
+}
+
+variable "env" {
+  type = string
+  description = "Nome do enviroment do recurso"
+}
